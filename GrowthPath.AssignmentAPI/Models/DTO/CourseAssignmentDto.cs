380c0a6b38de9b112e46aeac51398f661cd7add5
Nithesh Reddy Cheruku <288234@ust.com>
2024-11-16 09:38:21 +0530

    rem

--- GrowthPath.AssignmentAPI/Models/DTO/CourseAssignmentDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GRowthPath.AssignmentAPI.Models.DTO
-{
-    public class CourseAssignmentDto
-    {
-
-        public string EmployeeId { get; set; }  // From Auth API UserId
-        public int CourseId { get; set; }
-    }
-}
